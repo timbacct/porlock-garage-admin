66f4aba16af9aebfe316bda2570c380eab8c62d0
Tim Baker <35565983+timbacct@users.noreply.github.com>
2023-03-30 14:09:57 +0000

    Filled in sample data

--- app.py
+++ app.py
@@ -2,10 +2,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+INVOICEHEADERS = [
+  {
+    'id': 1,
+    'customername': 'Fred',
+    'invoicenumber': 123445,
+    'phonenumber': '01643 847322'
+  },
+  {
+    'id': 2,
+    'customername': 'Jim',
+    'invoicenumber': 125645,
+    'phonenumber': '01643 123443'
+  },
+  {
+    'id': 3,
+    'customername': 'Jo',
+    'invoicenumber': 84884,
+    'phonenumber': '01643 453454'
+  }
+    ]
+
 
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  
+  return render_template('home.html', invoiceheaders=INVOICEHEADERS)
 
 
 if __name__ == "__main__":
